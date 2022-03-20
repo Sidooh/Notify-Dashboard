c8ea88b16004aef914b7fa255c7344ba8b8f9773
nabcellent <nabcellent.dev@gmail.com>
2022-03-20 23:10:02 +0300

    Implement caching with swr
    Signed-off-by: nabcellent <nabcellent.dev@gmail.com>

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import JSONPretty from 'react-json-pretty';
 
 const ErrorPage = ({error}) => {
     return (
         <div>
-            <h3>Error occurred!</h3>
+            <h3>An Error Occurred!</h3>
 
             <div>
-                {error}
+                <JSONPretty data={error}/>
             </div>
         </div>
     );
