3c13c59fdd6bb6c2e59bc9e3646a6ad829a2e585
nabcellent <nabcellent.dev@gmail.com>
2022-03-29 20:21:46 +0300

    Removed unnecesary waffles and disabled unused ones

--- src/components/Waffle.tsx
+++ src/components/Waffle.tsx
@@ -1,8 +1,89 @@
-import React from 'react';
-import {IMAGES} from '../constants';
-import {CONFIG} from '../config';
+import React, { ReactNode } from 'react';
+import { IMAGES } from '../constants';
+import { CONFIG } from '../config';
+
+type WaffleItemType = {
+    label: string
+    icon: ReactNode,
+    href?: string
+    disabled?: boolean
+}
+
+const WaffleItem = ({label, icon, href, disabled = false}: WaffleItemType) => {
+    return (
+        <div className="col-4">
+            <div
+                className={ `d-block ${ disabled ? 'bg-100 ' : 'hover-bg-200 cursor-pointer rounded-3' } px-2 py-3 text-center text-decoration-none` }
+                onClick={ () => disabled ? false : window.open(href) }>
+                <div className="avatar avatar-2xl">
+                    { icon }
+                </div>
+                <p className="mb-0 fw-medium text-800 text-truncate fs--2">{ label }</p>
+            </div>
+        </div>
+    );
+};
 
 const Waffle = () => {
+    const serviceItems: WaffleItemType[] = [
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-light text-primary">
+                <span className="fs-2">L</span>
+            </div>,
+            label: 'Legacy',
+            href: CONFIG.sidooh.services.legacy.dashboard.url
+        },
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-primary text-primary">
+                <span className="fs-2">A</span>
+            </div>,
+            label: 'Accounts',
+            disabled: true
+        },
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-primary text-primary">
+                <span className="fs-2">N</span>
+            </div>,
+            label: 'Notify',
+            disabled: true
+        },
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-primary text-primary">
+                <span className="fs-2">S</span>
+            </div>,
+            label: 'Savings',
+            disabled: true
+        },
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-primary text-primary">
+                <span className="fs-2">P</span>
+            </div>,
+            label: 'Products',
+            disabled: true
+        },
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-primary text-primary">
+                <span className="fs-2">P</span>
+            </div>,
+            label: 'Payments',
+            disabled: true
+        },
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-primary text-primary">
+                <span className="fs-2">E</span>
+            </div>,
+            label: 'Enterprise',
+            disabled: true
+        },
+        {
+            icon: <div className="avatar-name rounded-circle bg-soft-primary text-primary">
+                <span className="fs-2">U</span>
+            </div>,
+            label: 'USSD',
+            disabled: true
+        }
+    ];
+
     return (
         <li className="nav-item dropdown">
             <span className="nav-link fa-icon-wait nine-dots p-1" id="navbarDropdownMenu" role="button"
@@ -27,133 +108,13 @@ const Waffle = () => {
                     <div className="scrollbar-overlay nine-dots-dropdown">
                         <div className="card-body px-3">
                             <div className="row text-center gx-0 gy-0">
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href={CONFIG.sidooh.services.legacy.dashboard.url} target="_blank"
-                                       rel="noreferrer">
-                                        <div className="avatar avatar-2xl">
-                                            <div className="avatar-name rounded-circle bg-soft-light text-primary">
-                                                <span className="fs-2">L</span>
-                                            </div>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">Legacy</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="app/events/event-detail.html" target="_blank">
-                                        <div className="avatar avatar-2xl">
-                                            <div className="avatar-name rounded-circle bg-soft-primary text-primary">
-                                                <span className="fs-2">A</span>
-                                            </div>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">Accounts</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="app/events/event-detail.html" target="_blank">
-                                        <div className="avatar avatar-2xl">
-                                            <div className="avatar-name rounded-circle bg-soft-dark text-primary">
-                                                <span className="fs-2">S</span>
-                                            </div>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">Savings</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="app/events/event-detail.html" target="_blank">
-                                        <div className="avatar avatar-2xl">
-                                            <div className="avatar-name rounded-circle bg-soft-info text-primary">
-                                                <span className="fs-2">P</span>
-                                            </div>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">Products</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="app/events/event-detail.html" target="_blank">
-                                        <div className="avatar avatar-2xl">
-                                            <div className="avatar-name rounded-circle bg-soft-danger text-primary">
-                                                <span className="fs-2">P</span>
-                                            </div>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">Payments</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="app/events/event-detail.html" target="_blank">
-                                        <div className="avatar avatar-2xl">
-                                            <div className="avatar-name rounded-circle bg-soft-secondary text-primary">
-                                                <span className="fs-2">E</span>
-                                            </div>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">Enterprise</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="app/events/event-detail.html" target="_blank">
-                                        <div className="avatar avatar-2xl">
-                                            <div className="avatar-name rounded-circle bg-soft-success text-primary">
-                                                <span className="fs-2">U</span>
-                                            </div>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">USSD</p>
-                                    </a>
-                                </div>
+                                {
+                                    serviceItems.map((item, i) => (
+                                        <WaffleItem key={ i } label={ item.label } icon={ item.icon } href={ item.href }
+                                                    disabled={ item.disabled }/>
+                                    ))
+                                }
 
-                                <div className="col-12">
-                                    <hr className="my-3 mx-n3 bg-200"/>
-                                </div>
-
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="pages/user/profile.html" target="_blank">
-                                        <div className="avatar avatar-2xl">
-                                            <img className="rounded-circle" src={IMAGES.team['2']} alt=""/>
-                                        </div>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2">Account</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="#!" target="_blank">
-                                        <img className="rounded" src={IMAGES.navIcons.trello} alt="" width="40"
-                                             height="40"/>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2 pt-1">Kanban</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="#!" target="_blank">
-                                        <img className="rounded" src={IMAGES.navIcons.slack} alt="" width="40"
-                                             height="40"/>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2 pt-1">Slack</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="#!" target="_blank">
-                                        <img className="rounded" src={IMAGES.navIcons.google} alt="" width="40"
-                                             height="40"/>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2 pt-1">Google</p>
-                                    </a>
-                                </div>
-                                <div className="col-4">
-                                    <a className="d-block hover-bg-200 px-2 py-3 rounded-3 text-center text-decoration-none"
-                                       href="https://github.com/Sidooh" target="_blank" rel="noopener noreferrer">
-                                        <img className="rounded" src={IMAGES.navIcons.github_light} alt="" width="40"
-                                             height="40"/>
-                                        <p className="mb-0 fw-medium text-800 text-truncate fs--2 pt-1">Github</p>
-                                    </a>
-                                </div>
-                                <div className="col-12">
-                                    <a className="btn btn-outline-primary btn-sm mt-4" href="#!">Show more</a>
-                                </div>
                             </div>
                         </div>
                     </div>
