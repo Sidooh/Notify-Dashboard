e36cd84408051e0af170ac0d20ebf4328e02ab7f
Nabcellent <nabcellent.dev@gmail.com>
2022-07-12 11:35:05 +0300

    Vite fixes

--- src/pages/notifications/Sms.tsx
+++ src/pages/notifications/Sms.tsx
@@ -16,74 +16,70 @@ const Sms = () => {
 
     let {data: notifications, isLoading, isSuccess, isError, error} = useNotificationsQuery();
 
-    return (
-        <>
-            {isError && <SectionError error={error}/>}
-            {!isLoading && isSuccess && notifications
-                ?
-                <Card>
-                    <Card.Body>
-                        <DataTable onCreateRow={() => navigate('/notifications/create')} title={'SMS Notifications'}
-                                   columns={[
-                                       {
-                                           accessorKey: 'destination',
-                                           header: 'Destination',
-                                           cell: (rowData: any) => <Destination notification={rowData.row.original}/>
-                                       },
-                                       {
-                                           accessorKey: 'message',
-                                           header: 'Message',
-                                           cell: (rowData: any) => {
-                                               const {message} = rowData.row.original;
-                                               return <Typography variant={"body2"} title={message} style={{
-                                                   display: "-webkit-box",
-                                                   overflow: "hidden",
-                                                   WebkitBoxOrient: "vertical",
-                                                   WebkitLineClamp: 2,
-                                                   cursor: "context-menu",
-                                                   maxWidth: '30rem'
-                                               }}>{message}</Typography>;
-                                           }
-                                       },
-                                       {
-                                           accessorKey: 'date',
-                                           header: 'Date',
-                                           cell: (rowData: any) => {
-                                               const {created_at} = rowData.row.original;
-                                               let date;
-                                               if (isToday(moment(created_at))) {
-                                                   date = "Today";
-                                               } else if (isYesterday(moment(created_at))) {
-                                                   date = "Yesterday";
-                                               } else {
-                                                   date = moment(created_at).format("D.M.y");
-                                               }
+    if (isError) return <SectionError error={error}/>;
+    if (isLoading || !isSuccess || !notifications) return <SectionLoader/>;
 
-                                               return <div style={{textAlign: "end"}}>
-                                                   <strong>{moment(created_at).format("LTS")}</strong><br/>
-                                                   <Typography variant={"caption"}>{date}</Typography>
-                                               </div>;
-                                           }
-                                       },
-                                       {
-                                           id: 'actions',
-                                           cell: (rowData: any) => {
-                                               const {id} = rowData.row.original;
-                                               return <Link to={`/notifications/${id}`}><ReadMore
-                                                   fontSize={'small'}/></Link>;
-                                           }
+    return (
+        <Card>
+            <Card.Body>
+                <DataTable onCreateRow={() => navigate('/notifications/create')} title={'SMS Notifications'}
+                           columns={[
+                               {
+                                   accessorKey: 'destination',
+                                   header: 'Destination',
+                                   cell: (rowData: any) => <Destination notification={rowData.row.original}/>
+                               },
+                               {
+                                   accessorKey: 'message',
+                                   header: 'Message',
+                                   cell: (rowData: any) => {
+                                       const {message} = rowData.row.original;
+                                       return <Typography variant={"body2"} title={message} style={{
+                                           display: "-webkit-box",
+                                           overflow: "hidden",
+                                           WebkitBoxOrient: "vertical",
+                                           WebkitLineClamp: 2,
+                                           cursor: "context-menu",
+                                           maxWidth: '30rem'
+                                       }}>{message}</Typography>;
+                                   }
+                               },
+                               {
+                                   accessorKey: 'date',
+                                   header: 'Date',
+                                   cell: (rowData: any) => {
+                                       const {created_at} = rowData.row.original;
+                                       let date;
+                                       if (isToday(moment(created_at))) {
+                                           date = "Today";
+                                       } else if (isYesterday(moment(created_at))) {
+                                           date = "Yesterday";
+                                       } else {
+                                           date = moment(created_at).format("D.M.y");
                                        }
-                                   ]} data={notifications.map(notification => {
-                            return {
-                                ...notification,
-                                message: notification.content,
-                            };
-                        })}/>
-                    </Card.Body>
-                </Card>
-                : <SectionLoader/>
-            }
-        </>
+
+                                       return <div style={{textAlign: "end"}}>
+                                           <strong>{moment(created_at).format("LTS")}</strong><br/>
+                                           <Typography variant={"caption"}>{date}</Typography>
+                                       </div>;
+                                   }
+                               },
+                               {
+                                   id: 'actions',
+                                   cell: (rowData: any) => {
+                                       const {id} = rowData.row.original;
+                                       return <Link to={`/notifications/${id}`}><ReadMore
+                                           fontSize={'small'}/></Link>;
+                                   }
+                               }
+                           ]} data={notifications.map(notification => {
+                    return {
+                        ...notification,
+                        message: notification.content,
+                    };
+                })}/>
+            </Card.Body>
+        </Card>
     );
 };
 
